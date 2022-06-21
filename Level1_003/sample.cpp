da21b40774e767e440ee7f5b21a85363998dc0c1
dd_zz6 <dd_zz6@naver.com>
2022-06-21 15:45:33 +0900

    //프로그래머스_Level1_핸드폰 번호 가리기

---
+++ Level1_003/sample.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//프로그래머스_Level1_핸드폰 번호 가리기
+
+string solution(string phone_number)
+{
+    string answer = "";
+    string base = "*";
+    int i = 0;
+
+    for (i; i < phone_number.size() - 4; i++)
+    {
+        answer += base;
+    }
+    for (i; i < phone_number.size(); i++)
+    {
+        answer += phone_number.at(i);
+    }
+    return answer;
+}
