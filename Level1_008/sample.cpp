de10374b441752816ced33859a422a6a8b7e3b7a
dd_zz6 <dd_zz6@naver.com>
2022-06-21 17:20:09 +0900

    프로그래머스_Level1_짝수와 홀수

---
+++ Level1_008/sample.cpp
@@ -0,0 +1,22 @@
+//프로그래머스_Level1_짝수와 홀수
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num)
+{
+    string answer = "";
+
+    if (num % 2 == 0)
+    {
+        answer = "Even";
+    }
+    else
+    {
+        answer = "Odd";
+    }
+
+    return answer;
+}
