7610d64a415f2df1b026119fa639a72f67c00747
dd_zz6 <dd_zz6@naver.com>
2022-06-21 21:26:47 +0900

    프로그래머스_Level1_시저 암호

---
+++ Level1_016/sample.cpp
@@ -0,0 +1,31 @@
+//프로그래머스_Level1_시저 암호
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) 
+{
+    string answer = "";
+    string temp = "";
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            temp = ' ';
+        }
+        else if (s[i] + n > 122)
+        {
+            temp = s[i] + n - 26;
+        }
+        else
+        {
+            temp = s[i] + n;
+        }
+        answer += temp;
+    }
+
+    return answer;
+}
